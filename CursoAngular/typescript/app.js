ccf254497925a9d7367954641ead248586fcd02a
Alejandro Sugich <115648645+asugich@users.noreply.github.com>
2023-03-08 23:34:56 -0700

    Add files via upload

---
+++ CursoAngular/typescript/app.js
@@ -0,0 +1,11 @@
+"use strict";
+(() => {
+    const avenger = {
+        nombre: 'Steve',
+        clave: 'Capitán América',
+        poder: 'Droga'
+    };
+    console.log(avenger.nombre);
+    console.log(avenger.clave);
+    console.log(avenger.poder);
+})();
---
+++ CursoAngular/typescript/app.ts
@@ -0,0 +1,14 @@
+(() => {
+
+    const avenger = {
+        nombre: 'Steve',
+        clave: 'Capitán América',
+        poder: 'Droga'
+    }
+
+    console.log(avenger.nombre);
+    console.log(avenger.clave);
+    console.log(avenger.poder);
+
+})();
+
